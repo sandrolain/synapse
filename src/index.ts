98ec6992b0a043da0d643f3e2f65053a4228bf74
Sandro Lain <sandrolain@MBP-di-Sandro.station>
2020-03-24 07:50:57 +0100

    Code linting

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-/* eslint @typescript-eslint/no-explicit-any: 0 */
-
 export type Subscriber = ((data: any) => void) | Emitter;
 export type Processor = (data: any) => any;
 export interface IntervalData {
@@ -12,25 +10,25 @@ export interface IntervalData {
 export class Emitter {
   private subscriptions: Set<Subscriber> = new Set();
 
-  constructor(private processor: Processor = (data: any): any => data) {}
+  constructor (private processor: Processor = (data: any): any => data) {}
 
-  subscribe(subscriber: Subscriber): void {
+  subscribe (subscriber: Subscriber): void {
     this.subscriptions.add(subscriber);
   }
 
-  unsubscribe(subscriber: Subscriber): void {
+  unsubscribe (subscriber: Subscriber): void {
     this.subscriptions.delete(subscriber);
   }
 
-  subscribed(subscriber: Subscriber): boolean {
+  subscribed (subscriber: Subscriber): boolean {
     return this.subscriptions.has(subscriber);
   }
 
-  subscribeTo(emitter: Emitter): void {
+  subscribeTo (emitter: Emitter): void {
     return emitter.subscribe(this);
   }
 
-  promise(): Promise<any> {
+  promise (): Promise<any> {
     return new Promise(resolve => {
       const fn = (data: any): void => {
         this.unsubscribe(fn);
@@ -42,30 +40,30 @@ export class Emitter {
     });
   }
 
-  private async prepareData(data: any): Promise<any> {
+  private async prepareData (data: any): Promise<any> {
     let processedData = this.processor(data);
 
-    if (processedData instanceof Promise) {
+    if(processedData instanceof Promise) {
       processedData = await processedData;
     }
 
     return processedData;
   }
 
-  async emit(data: any = null): Promise<void> {
+  async emit (data: any = null): Promise<void> {
     const processedData = await this.prepareData(data);
 
-    for (const sub of this.subscriptions) {
-      if (sub instanceof Emitter) {
+    for(const sub of this.subscriptions) {
+      if(sub instanceof Emitter) {
         sub.emit(processedData);
-      } else if (typeof sub === "function") {
+      } else if(typeof sub === "function") {
         sub.call(this, processedData);
       }
     }
   }
 
-  async emitAll(dataList: any[]): Promise<void> {
-    for (const data of dataList) {
+  async emitAll (dataList: any[]): Promise<void> {
+    for(const data of dataList) {
       await this.emit(data);
     }
   }
@@ -73,7 +71,7 @@ export class Emitter {
   private tou: number;
   private itv: number;
 
-  async emitInterval(
+  async emitInterval (
     delay: number,
     interval: number,
     maxTimes: number = 0
@@ -89,7 +87,7 @@ export class Emitter {
         maxTimes
       } as IntervalData);
 
-      if (maxTimes > 0 && maxTimes === times) {
+      if(maxTimes > 0 && maxTimes === times) {
         return;
       }
 
@@ -102,30 +100,30 @@ export class Emitter {
           maxTimes
         } as IntervalData);
 
-        if (maxTimes > 0 && maxTimes === times) {
+        if(maxTimes > 0 && maxTimes === times) {
           this.stopInterval();
         }
       }, interval);
     }, delay);
   }
 
-  stopInterval() {
-    if (this.tou) {
+  stopInterval (): void {
+    if(this.tou) {
       window.clearTimeout(this.tou);
       this.tou = null;
     }
 
-    if (this.itv) {
+    if(this.itv) {
       window.clearInterval(this.itv);
       this.itv = null;
     }
   }
 
-  filter(filterFn: (data: any) => boolean): Emitter {
+  filter (filterFn: (data: any) => boolean): Emitter {
     const emitter = new Emitter();
 
     this.subscribe((data: any) => {
-      if (filterFn(data)) {
+      if(filterFn(data)) {
         emitter.emit(data);
       }
     });
@@ -133,7 +131,7 @@ export class Emitter {
     return emitter;
   }
 
-  map(mapFn: (data: any) => any): Emitter {
+  map (mapFn: (data: any) => any): Emitter {
     const emitter = new Emitter();
 
     this.subscribe((data: any) => {
@@ -144,7 +142,7 @@ export class Emitter {
     return emitter;
   }
 
-  reduce(
+  reduce (
     reduceFn: (accumData: any, itemData: any) => any,
     accumData: any
   ): Emitter {
@@ -159,7 +157,7 @@ export class Emitter {
     return emitter;
   }
 
-  delay(delay: number): Emitter {
+  delay (delay: number): Emitter {
     const emitter = new Emitter();
 
     this.subscribe((data: any) => {
@@ -169,12 +167,12 @@ export class Emitter {
     return emitter;
   }
 
-  debounce(releaseEmitter: Emitter): Emitter {
+  debounce (releaseEmitter: Emitter): Emitter {
     const emitter = new Emitter();
     let ok: boolean = false;
 
     this.subscribe((data: any) => {
-      if (ok) {
+      if(ok) {
         emitter.emit(data);
         ok = false;
       }
@@ -187,13 +185,13 @@ export class Emitter {
     return emitter;
   }
 
-  debounceTime(time: number): Emitter {
+  debounceTime (time: number): Emitter {
     const emitter = new Emitter();
     let ok: boolean = false;
     let emitterTO: any;
 
     this.subscribe((data: any) => {
-      if (ok) {
+      if(ok) {
         emitter.emit(data);
         ok = false;
       }
@@ -206,7 +204,7 @@ export class Emitter {
     return emitter;
   }
 
-  audit(releaseEmitter: Emitter): Emitter {
+  audit (releaseEmitter: Emitter): Emitter {
     const emitter = new Emitter();
     let lastData: any = null;
 
@@ -221,7 +219,7 @@ export class Emitter {
     return emitter;
   }
 
-  auditTime(time: number): Emitter {
+  auditTime (time: number): Emitter {
     const emitter = new Emitter();
     let lastData: any = undefined;
     let emitterTO: any;
@@ -232,7 +230,7 @@ export class Emitter {
       clearTimeout(emitterTO);
 
       emitterTO = setTimeout(() => {
-        if (lastData !== undefined) {
+        if(lastData !== undefined) {
           emitter.emit(lastData);
         }
       }, time);
@@ -241,7 +239,7 @@ export class Emitter {
     return emitter;
   }
 
-  buffer(releaseEmitter: Emitter): Emitter {
+  buffer (releaseEmitter: Emitter): Emitter {
     const emitter = new Emitter();
     let bufferData: any[] = [];
 
