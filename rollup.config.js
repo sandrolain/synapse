85db161cf04b9e8c0ab7bf5f6e5a4be25ab82e69
Sandro Lain <sandrolain@MBP-di-Sandro.station>
2020-04-09 18:19:27 +0200

    Depndencies, package.json and rollup configuration updates

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,45 @@
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
-import pkg from "./package.json";
+import { terser } from "rollup-plugin-terser";
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: pkg.main,
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/umd/index.js",
       format: "umd",
-      name: "synapse"
+      name: "fancyCase",
+      esModule: false,
+      sourcemap: true
     },
-    {
-      file: pkg.module,
-      format: "es",
-      name: "synapse"
-    }
-  ],
-  plugins: [
-    typescript({
-      typescript: require("typescript")
-    }),
-    resolve()
-  ]
-};
+    plugins: [
+      typescript({
+        typescript: require("typescript")
+      }),
+      terser()
+    ]
+  },
+  {
+    input: {
+      index: "src/index.ts"
+    },
+    output: [
+      {
+        dir: "./dist/cjs",
+        format: "cjs",
+        sourcemap: true
+      },
+      {
+        dir: "./dist/esm",
+        format: "esm",
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      typescript({
+        typescript: require("typescript")
+      }),
+      resolve()
+    ]
+  }
+];
